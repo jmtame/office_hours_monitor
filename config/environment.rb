1950bf082128cc940a1a4a48b9db727e31fc0b17
Jared Tame <jmtame@gmail.com>
2012-09-10 04:24:37 -0700

    attempt to fix mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 OfficeHoursMonitor::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => "sendgrid.me",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
