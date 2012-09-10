4075b04e50ec109faab53e6560b26518b10e141e
Jared Tame <jmtame@gmail.com>
2012-09-10 13:29:37 -0700

    different mailer method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ OfficeHoursMonitor::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :domain               => 'heroku.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
